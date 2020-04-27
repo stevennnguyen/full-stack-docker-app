550b9e07736979a4f352b484a80c75722e15097d
stovin <stovin@stovins-MBP.T-mobile.com>
2020-04-27 15:59:28 -0700

    Added post functionality. Works in sqlite, mssql functionality needs to be updated to reflected all the column rows.

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,12 +1,23 @@
-from dataclasses import dataclass
-from flask import Flask, jsonify
+#from dataclasses import dataclass
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
+# Init app
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:1234testdb!@sql-server-db:1433/WideWorldImporters?driver=ODBC+Driver+17+for+SQL+Server'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
 db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
 
-@dataclass
+# Warehouse Color Class/Model
+#@dataclass
 class WarehouseColor(db.Model):
     __table_args__ = {'schema':'Warehouse'}
     __tablename__ = 'Colors'
@@ -14,13 +25,48 @@ class WarehouseColor(db.Model):
     id: int
     data: str
 
+    #id = db.Column('ColorID', db.Integer, primary_key=True)
+    #data = db.Column('ColorName', db.String(25))
+
     id = db.Column('ColorID', db.Integer, primary_key=True)
-    data = db.Column('ColorName', db.String(25))
+    color = db.Column('ColorName', db.String(25))
+
+    def __init__(self, color):
+        self.color = color
+
+# Warehouse Schema
+class WarehouseColorSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'color')
+
+# Init schema
+warehousecolor_schema = WarehouseColorSchema()
+warehousecolors_schema = WarehouseColorSchema(many=True)
+
+# Create a Warehouse Color
+@app.route('/warehouse/color', methods=['POST'])
+def add_color():
+    color = request.json['color']
+
+    new_color = WarehouseColor(color)
+
+    db.session.add(new_color)
+    db.session.commit()
+
+    return warehousecolor_schema.jsonify(new_color)
+
+# Get All Warehouse Colors
+@app.route('/warehouse/color', methods=['GET'])
+def get_color():
+    all_colors = WarehouseColor.query.all()
+    result = warehousecolors_schema.dump(all_colors)
+    return jsonify(result)
 
 @app.route('/')
 def index():
     return 'Index Page'
 
+"""
 @app.route('/warehouse/colors')
 def colors(): 
     return jsonify(WarehouseColor.query.all())
@@ -28,6 +74,7 @@ def colors():
 @app.route('/warehouse/colors/<colorid>')
 def color(colorid):
     return jsonify(WarehouseColor.query.filter_by(id=colorid).first())
+"""
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
