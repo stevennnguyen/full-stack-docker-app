2a2866cc129fd9f583b000378917b9590d2660e3
stovin <stovin@stovins-MBP.T-mobile.com>
2020-05-01 14:09:59 -0700

    Additional REST fuctionality

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,4 +1,3 @@
-#from dataclasses import dataclass
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -7,10 +6,11 @@ import os
 
 # Init app
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
+# Local database
+#basedir = os.path.abspath(os.path.dirname(__file__))
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:1234testdb!@sql-server-db:1433/WideWorldImporters?driver=ODBC+Driver+17+for+SQL+Server'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Init db
 db = SQLAlchemy(app)
@@ -18,33 +18,23 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 # Warehouse Color Class/Model
-#@dataclass
 class WarehouseColor(db.Model):
+    # Database
     __table_args__ = {'schema':'Warehouse'}
     __tablename__ = 'Colors'
-
-    #id: int
-    #data: str
-
-    #id = db.Column('ColorID', db.Integer, primary_key=True)
-    #data = db.Column('ColorName', db.String(25))
 
     id = db.Column('ColorID', db.Integer, primary_key=True)
     color = db.Column('ColorName', db.String(25))
     lasteditedby = db.Column('LastEditedBy', db.Integer)
-    #validfrom = db.Column('ValidFrom', db.DateTime, default=datetime.utcnow)
-    #validto = db.Column('ValidTo', db.DateTime, default=datetime.utcnow)
 
-    def __init__(self, color, lasteditedby):#, validfrom, validto):
+    def __init__(self, color, lasteditedby):
         self.color = color
         self.lasteditedby = lasteditedby
-        #self.validfrom = validfrom
-        #self.validto = validto
 
 # Warehouse Schema
 class WarehouseColorSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'color', 'lasteditedby')#, 'validfrom', 'validto')
+        fields = ('id', 'color', 'lasteditedby')
 
 # Init schema
 warehousecolor_schema = WarehouseColorSchema()
@@ -55,10 +45,8 @@ warehousecolors_schema = WarehouseColorSchema(many=True)
 def add_color():
     color = request.json['color']
     lasteditedby = request.json['lasteditedby']
-    #validfrom = request.json['validfrom']
-    #validto = request.json['validto']
 
-    new_color = WarehouseColor(color, lasteditedby)#, validfrom, validto)
+    new_color = WarehouseColor(color, lasteditedby)
 
     db.session.add(new_color)
     db.session.commit()
@@ -67,24 +55,43 @@ def add_color():
 
 # Get All Warehouse Colors
 @app.route('/warehouse/color', methods=['GET'])
-def get_color():
+def get_colors():
     all_colors = WarehouseColor.query.all()
     result = warehousecolors_schema.dump(all_colors)
     return jsonify(result)
 
+# Get Single Warehouse Color
+@app.route('/warehouse/color/<id>', methods=['GET'])
+def get_color(id):
+    color = WarehouseColor.query.get(id)
+    return warehousecolor_schema.jsonify(color)
+
+# Update a Product
+@app.route('/warehouse/color/<id>', methods=['PUT'])
+def update_color(id):
+    warehousecolor = WarehouseColor.query.get(id)
+
+    color = request.json['color']
+    lasteditedby = request.json['lasteditedby']
+
+    warehousecolor.color = color
+    warehousecolor.lasteditedby = lasteditedby
+
+    db.session.commit()
+
+    return warehousecolor_schema.jsonify(warehousecolor)
+
+# Delete Warehouse Color
+@app.route('/warehouse/color/<id>', methods=['DELETE'])
+def delete_color(id):
+    color = WarehouseColor.query.get(id)
+    db.session.delete(color)
+    db.session.commit()
+    return warehousecolor_schema.jsonify(color)
+
 @app.route('/')
 def index():
     return 'Index Page'
-
-"""
-@app.route('/warehouse/colors')
-def colors(): 
-    return jsonify(WarehouseColor.query.all())
-
-@app.route('/warehouse/colors/<colorid>')
-def color(colorid):
-    return jsonify(WarehouseColor.query.filter_by(id=colorid).first())
-"""
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
